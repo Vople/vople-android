86f75d160a3b49cd8ead244f5e98eb65fe978bd3
Youshin Kim <980513k@gmail.com>
2018-11-05 01:25:19 +0900

    login.xml , signup.xml

--- app/src/main/java/com/mobile/vople/vople/SplashActivity.java
+++ app/src/main/java/com/mobile/vople/vople/SplashActivity.java
@@ -12,9 +12,7 @@ public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_screen);
-
-
+        setContentView(R.layout.activity_splash);
 
         new Handler().postDelayed(new Runnable(){
             @Override
